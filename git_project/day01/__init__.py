f10d857c4a29b5056e790226f21756cd9402549d
panhub10 <253724363@qq.com>
2022-06-14 23:03:39 +0800

    提交helloworld

---
+++ git_project/day01/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/6/14 23:02
+Copyright (c) 2022/6/14, 海牛学院版权所有.
+@author: 潘牛
+"""
---
+++ git_project/day01/hello_world.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+hello_world.py
+Created on 2022/6/14 23:02
+Copyright (c) 2022/6/14, 海牛学院版权所有.
+@author: 潘牛
+"""
+print("hello world")
