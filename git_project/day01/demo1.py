6d501404e60cf5e90f05e4dc44c04e95382876e5
panhub10 <253724363@qq.com>
2022-06-15 00:02:58 +0800

    修改文件内容+增加新文件

---
+++ git_project/day01/demo1.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+demo1.py
+Created on 2022/6/15 0:00
+Copyright (c) 2022/6/15, 海牛学院版权所有.
+@author: 潘牛
+"""
+a = 10
+b = 11
+print(a + b)
--- git_project/day01/hello_world.py
+++ git_project/day01/hello_world.py
@@ -7,3 +7,4 @@ Copyright (c) 2022/6/14, 海牛学院版权所有.
 """
 print("hello world")
 print("hello world")
+print("hello world")
