627964d16f3cc805ae3c7d3b0fdb17a4338b125a
panhub10 <253724363@qq.com>
2022-09-30 07:43:41 +0800

    提交工程git_project2

---
+++ git_project2/day01/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/9/30 7:42
+Copyright (c) 2022/9/30, 海牛学院版权所有.
+@author: 潘牛
+"""
---
+++ git_project2/day01/m1.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+m1.py
+Created on 2022/9/30 7:43
+Copyright (c) 2022/9/30, 海牛学院版权所有.
+@author: 潘牛
+"""
+a = 1
+b = 2
